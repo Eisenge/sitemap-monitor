de8582bba082c1d32b8dd480c871724022b78129
Eisenge <lishihchiehn@gmail.com>
2026-08-13 20:46:46 +0800

    upgrade dashboard v5

--- docs/app.js
+++ docs/app.js
@@ -1,8 +1,1 @@
-async function load(){
-let g=await fetch('../data/groups.json').then(r=>r.json());
-let w=await fetch('../data/websites.json').then(r=>r.json());
-document.getElementById('app').innerHTML=g.map(x=>'<div class="card"><h2>'+x.name+'</h2>'+w.filter(y=>y.group==x.name).map(y=>y.name+' '+y.url).join('<br>')+'</div>').join('');
-}
-function addGroup(){alert('升级版接口预留');}
-function addSite(){alert('升级版接口预留');}
-load();
+async function load(){let g=await fetch('../data/groups.json').then(r=>r.json());let w=await fetch('../data/websites.json').then(r=>r.json());document.getElementById('app').innerHTML=g.map(x=>'<div class="card"><h2>'+x.name+'</h2>'+w.filter(y=>y.group==x.name).map(y=>y.name+'<br>'+y.url).join('')+'</div>').join('')}load();
