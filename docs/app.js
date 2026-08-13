a821b3dc7910ad823bbf6211515fa56f79d3c87b
Eisenge <lishihchiehn@gmail.com>
2026-08-13 20:58:56 +0800

    upgrade dashboard v6

--- docs/app.js
+++ docs/app.js
@@ -1,1 +1,54 @@
-async function load(){let g=await fetch('../data/groups.json').then(r=>r.json());let w=await fetch('../data/websites.json').then(r=>r.json());document.getElementById('app').innerHTML=g.map(x=>'<div class="card"><h2>'+x.name+'</h2>'+w.filter(y=>y.group==x.name).map(y=>y.name+'<br>'+y.url).join('')+'</div>').join('')}load();
+async function loadData(){
+
+const groups = await fetch('/sitemap-monitor/data/groups.json')
+.then(res=>res.json());
+
+const websites = await fetch('/sitemap-monitor/data/websites.json')
+.then(res=>res.json());
+
+
+let html = "";
+
+groups.forEach(group=>{
+
+let sites = websites.filter(
+w=>w.group === group.name
+);
+
+
+html += `
+<div class="card">
+
+<h2>${group.name}</h2>
+
+<p>
+网站数量:
+${sites.length}
+</p>
+
+${sites.map(site=>`
+
+<div class="site">
+
+<b>${site.name}</b>
+
+<br>
+
+${site.url}
+
+</div>
+
+`).join("")}
+
+</div>
+`;
+
+});
+
+
+document.getElementById("app").innerHTML = html;
+
+}
+
+
+loadData();
